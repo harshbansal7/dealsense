11d8c82df4c22e952e5bbc8847b90eab1171b801
harshbansal7 <harshbansal1036@gmail.com>
2025-09-21 19:26:37 +0530

    vercel changes

--- dealsense-manager/frontend/next.config.ts
+++ dealsense-manager/frontend/next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
   output: 'standalone',
+  outputFileTracingRoot: path.join(__dirname, '../../'),
   images: {
     remotePatterns: [
       {
@@ -17,6 +19,13 @@ const nextConfig: NextConfig = {
         pathname: '/wikipedia/commons/**',
       },
     ],
+  },
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.resolve(__dirname, 'src'),
+    };
+    return config;
   },
 };
 
